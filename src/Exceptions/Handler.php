7acafcd6c444360a5265c5f4f5ce7f78650eec1a
Charles Louw <charles.louw@gmail.com>
2019-04-24 11:35:20 +0200

    feat: remove exception instances which should be added to the App\Exceptions\Exceptions::$dontReport property

--- src/Exceptions/Handler.php
+++ src/Exceptions/Handler.php
@@ -14,10 +14,6 @@ class Handler {
 		if ($exception instanceof HttpExceptionInterface) {
 			// Handle any HTTP exception
 			HttpResponse::log($exception->getMessage(), $exception->getStatusCode());
-		} elseif ($exception instanceof ValidationException) {
-			// Don't need to log validation exceptions - problematic fields are displayed to user on the screen
-		} elseif ($exception instanceof AuthenticationException) {
-			// Don't need to log because user will get authentication error message on screen
 		} else {
 			// Anything not already captured should be logged as a 500 error
 			HttpResponse::log($exception->getMessage(), 500);
